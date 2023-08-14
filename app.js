3f880ce273cf5b17ce51cfb7fc8ed86ec8f395db
Kot <katkova.alla@gmail.com>
2023-08-14 21:16:08 +0300

    feat: роут на страницу, которой не существует

--- app.js
+++ app.js
@@ -33,4 +33,10 @@ app.use('/users', require('./routes/users'));
 
 app.use('/cards', require('./routes/cards'));
 
+// если запрос идет на неизвестный роут
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.listen(PORT);
