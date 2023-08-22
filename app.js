881c870d0274d6416ae0549d08bde00589a013a4
Kot <katkova.alla@gmail.com>
2023-08-22 20:02:24 +0300

    feat: централизация ошибок в апп, создание общего роута,создание папки и 2х видов ошибок 400 и 404

--- app.js
+++ app.js
@@ -38,4 +38,18 @@ app.use('*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
+// централизация ошибок
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+    next();
+});
+
 app.listen(PORT);
