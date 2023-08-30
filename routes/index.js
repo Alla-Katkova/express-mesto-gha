4429db3ad7bcf69746c1b05b69d2be75ef13fb6d
Kot <katkova.alla@gmail.com>
2023-08-30 22:48:32 +0300

    feat:auth middleware

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const signupRouter = require('./signup');
 const signinRouter = require('./signin');
+const auth = require('../middlewares/auth')
 
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
